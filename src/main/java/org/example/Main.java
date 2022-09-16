0de39dd60416ec43c00dc43bc74924d3cd4d7780
Aaron Fisher <aaronfisher@cmail.carleton.ca>
2022-09-16 11:54:19 -0400

    Test Commit 1: Assert parameters are >0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int a;
+    public static int b;
+    public static int c;
+
+    public String kindOfTriangle (int a, int b, int c){
+
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Ensure inputs are positive")
+    void positive() {
+        assertAll(() -> assertTrue(Main.a > 0),
+                assertTrue(Main.b > 0),
+                assertTrue(Main.c > 0);
+
+    }
+}
